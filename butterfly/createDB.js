7549c6c84f5fa6e4ce781f5feffd1cb3d95d78f0
Safiyeshka <koroglu1.s.i.22@gmail.com>
2024-01-06 19:29:01 +0300

    6.1. Подключение базы данных

---
+++ butterfly/createDB.js
@@ -0,0 +1,17 @@
+var MongoClient = require('mongodb').MongoClient
+const uri = "mongodb://localhost:27017/"
+const client = new MongoClient(uri)
+async function run() {
+try {
+await client.connect();
+var database = client.db("butterfly");
+database.dropDatabase()
+database = client.db("butterfly");
+const butterflies = database.collection("butterflies");
+const result = await butterflies.insertOne({name:"Greta Oto"});
+console.log(`${result} documents were inserted`);
+} finally {
+await client.close();
+}
+}
+run()
