9f31b5fa429cb13269e5ed2eb3111ecbc233f9d9
Safiyeshka <koroglu1.s.i.22@gmail.com>
2024-01-06 19:36:51 +0300

    6.2. Создание модуля данных

---
+++ butterfly/data.js
@@ -0,0 +1,21 @@
+var data = [{
+        title: 'Cethosia Cyane',
+        nick: 'cethosia_cyane',
+        avatar: './images/cc.png',
+        desc: 'This butterfly species, whose males are orange and females are grey and white, lives in India and China.'
+    },
+    {
+        title: 'Greta Oto',
+        nick: 'greta_oto',
+        avatar: './images/go.png',
+        desc: 'Gre Oto butterflies with transparent wings live in countries such as Ecuador, Panama and Venezuela'
+    },
+    {
+        title: 'Papilio Memnon',
+        nick: 'papilio_memnon',
+        avatar: './images/pm.png',
+        desc: 'With a wing length of up to 150 millimetres, this butterfly species lives mainly in South Asia.'
+    }
+];
+
+module.exports.data = data;
