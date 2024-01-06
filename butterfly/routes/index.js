f2df184ed0335ea3d27a41b155e7ac949f264d17
Safiyeshka <koroglu1.s.i.22@gmail.com>
2024-01-06 18:50:06 +0300

    4.3. Создание шаблона

--- butterfly/routes/index.js
+++ butterfly/routes/index.js
@@ -6,30 +6,39 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* Страница Карамельки */
-router.get('/karamelka', function(req, res, next) {
-  res.send("<h1>Страница Карамельки</h1>")
-  });
 
 /*Cethosia Cyane*/
-
 router.get('/cethosia_cyane', function(req, res, next) {
-    res.send("<h1>Cethosia Cyane</h1>")
+  res.render('butterfly', {
+        title: "Cethosia Cyane",
+        picture: "images/cc1.png",
+        desc: "This butterfly species, whose males are orange and females are grey and white, lives in India and China.",
+        
   });
+});
 
 
 /*Greta Oto*/
-
 router.get('/greta_oto', function(req, res, next) {
-  res.send("<h1>Greta Oto</h1>")
+  res.render('butterfly', {
+        title: "Greta Oto",
+        picture: "images/go1.png",
+        desc: " Gre Oto butterflies with transparent wings live in countries such as Ecuador, Panama and Venezuela.",
+      
 });
-
+});
 
 /*Papilio Memnon*/
-
 router.get('/papilio_memnon', function(req, res, next) {
-  res.send("<h1>Papilio Memnon</h1>")
+  res.render('butterfly', {
+        title: "Papilio Memnon",
+        picture: "images/pm1.png",
+        desc: " With a wing length of up to 150 millimetres, this butterfly species lives mainly in South Asia.",
+
 });
+});
+
   
+ 
 
 module.exports = router;
