c511463510a614dc211808c6138a1a55fa8dc577
Safiyeshka <koroglu1.s.i.22@gmail.com>
2024-01-07 20:49:05 +0300

    11.4. Заполнение меню из базы

--- butterfly/middleware/createMenu.js
+++ butterfly/middleware/createMenu.js
@@ -3,7 +3,7 @@ const Butterfly = require("./../models/butterfly").Butterfly;
 module.exports = async function(req, res, next) {
     try {
       res.locals.nav = [];
-      const result = await Butterfly.find({}, { _id: 0, title: 1, nick: 1 });
+      const result = await Butterfly.find(null, { _id: 0, title: 1, nick: 1 });
       res.locals.nav = result;
       next();
 
