cb7567ce2a735640ef73a822ed4a64471cc4e0a9
Safiyeshka <koroglu1.s.i.22@gmail.com>
2024-01-07 11:36:34 +0300

    8.2. Обработка параметра в адресе

--- butterfly/app.js
+++ butterfly/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var butterflies = require('./routes/butterflies');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/butterflies', butterflies);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
