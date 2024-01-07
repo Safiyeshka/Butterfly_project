5db6000ae6eb4be90c940f04e19e62e4dc39a375
Safiyeshka <koroglu1.s.i.22@gmail.com>
2024-01-07 20:44:53 +0300

    11.3. Вывод данных на экранах

--- butterfly/routes/butterflies.js
+++ butterfly/routes/butterflies.js
@@ -1,41 +1,60 @@
 const express = require('express');
 const router = express.Router();
-const Butterfly = require("../models/butterfly").Butterfly;
+// const Butterfly = require("../models/butterfly").Butterfly;
 var async = require("async");
-var checkAuth = require("./../middleware/checkAuth.js")
+var db = require('../mySQLConnect.js');
+// var checkAuth = require("./../middleware/checkAuth.js")
 
-// /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('Новый маршрутизатор, для маршрутов, начинающихся с butterflies');
-// });
+router.get("/:nick", function(req, res, next) {
+  db.query(`SELECT * FROM butterflies WHERE butterflies.nick = '${req.params.nick}'`, (err, butterflies) => {
+  if(err) {
+    console.log(err);
+    if(err) return next(err)
+    } else {
+      if(butterflies.length == 0) return next(new Error("Page not found"))
+        var butterfly = butterflies[0];
+        res.render('butterfly', {
+          title: butterfly.title,
+          picture: butterfly.avatar,
+          desc: butterfly.about
+  })
+}
+})
 
-/* Страница бабочек */
-router.get('/:nick', checkAuth, async function(req, res, next) {
-    try {
-      const [butterfly, butterflies] = await Promise.all([
-        Butterfly.findOne({ nick: req.params.nick }),
-        Butterfly.find({}, { _id: 0, title: 1, nick: 1 })
-      ]);
+});
+
+// // /* GET users listing. */
+// // router.get('/', function(req, res, next) {
+// //   res.send('Новый маршрутизатор, для маршрутов, начинающихся с butterflies');
+// // });
+
+// /* Страница бабочек */
+// router.get('/:nick', checkAuth, async function(req, res, next) {
+//     try {
+//       const [butterfly, butterflies] = await Promise.all([
+//         Butterfly.findOne({ nick: req.params.nick }),
+//         Butterfly.find({}, { _id: 0, title: 1, nick: 1 })
+//       ]);
     
-      if (!butterfly) {
-        throw new Error("Page not found");
-      }
+//       if (!butterfly) {
+//         throw new Error("Page not found");
+//       }
       
-      renderButterfly(res, butterfly.title, butterfly.avatar, butterfly.desc, butterflies);
-    } catch (err) {
-      next(err);
-    }
-  });
+//       renderButterfly(res, butterfly.title, butterfly.avatar, butterfly.desc, butterflies);
+//     } catch (err) {
+//       next(err);
+//     }
+//   });
   
-  function renderButterfly(res, title, picture, desc, butterflies) {
-    console.log(butterflies);
+//   function renderButterfly(res, title, picture, desc, butterflies) {
+//     console.log(butterflies);
   
-    res.render('butterfly', {
-      title: title,
-      picture: picture,
-      desc: desc,
-    });
-  }
+//     res.render('butterfly', {
+//       title: title,
+//       picture: picture,
+//       desc: desc,
+//     });
+//   }
   
   
   module.exports = router;
