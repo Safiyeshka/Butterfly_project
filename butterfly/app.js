5d9f637626014a1d555dbcdaa248256f6b52dffd
Safiyeshka <koroglu1.s.i.22@gmail.com>
2024-01-07 21:08:54 +0300

    11.5. Создание сессии

--- butterfly/app.js
+++ butterfly/app.js
@@ -4,16 +4,28 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/butterfly')
+// var mongoose = require('mongoose')
+// mongoose.connect('mongodb://localhost/butterfly')
 
-var session = require("express-session")
+const session = require("express-session")
+var MySQLStore = require('express-mysql-session')(session); 
+var mysql2 = require('mysql2/promise');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var butterflies = require('./routes/butterflies');
 
 var app = express();
+var options = {
+  host : '127.0.0.1',
+  port: '3306',
+  user : 'root',
+  password : 'root',
+  database: 'butterfly'
+  };
+  
+  var connection = mysql2.createPool(options)
+  var sessionStore = new MySQLStore( options, connection);
 
 // view engine setup
 
@@ -27,15 +39,30 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var MongoStore = require('connect-mongo');
 app.use(session({
-  secret: "butterfly",
-  cookie:{maxAge:600*1000},
+  secret: 'butterfly',
+  key: 'sid',
+  store: sessionStore,
   resave: true,
   saveUninitialized: true,
-  // secure: true,
-  store: MongoStore.create({mongoUrl: 'mongodb://localhost/butterfly'})
-  }));
+  cookie: { path: '/',
+  httpOnly: true,
+  maxAge: 60*1000
+} }));
+
+
+// var MongoStore = require('connect-mongo');
+// app.use(session({
+//   secret: "butterfly",
+//   cookie:{maxAge:600*1000},
+//   resave: true,
+//   saveUninitialized: true,
+//   // secure: true,
+//   store: MongoStore.create({mongoUrl: 'mongodb://localhost/butterfly'})
+//   }));
+
+var MySQLStore = require('express-mysql-session')(session);
+
 app.use(function(req,res,next){
     req.session.counter = req.session.counter +1 || 1
     next()
--- butterfly/middleware/createMenu.js
+++ butterfly/middleware/createMenu.js
@@ -1,13 +1,28 @@
-const Butterfly = require("./../models/butterfly").Butterfly;
+const db = require('./../mySQLConnect'); // assuming MySQL database connection
 
-module.exports = async function(req, res, next) {
-    try {
-      res.locals.nav = [];
-      const result = await Butterfly.find(null, { _id: 0, title: 1, nick: 1 });
-      res.locals.nav = result;
-      next();
-
-    } catch (err) {
-      throw err;
-    }
+module.exports = function(req, res, next) {
+    res.locals.nav = [];
+    db.query('SELECT butterflies.title, butterflies.nick FROM butterflies', function(err, result) {
+        if (err) throw err;
+        res.locals.nav = result;
+        next();
+    });
 };
+
+
+
+
+
+// const Butterfly = require("./../models/butterfly").Butterfly;
+
+// module.exports = async function(req, res, next) {
+//     try {
+//       res.locals.nav = [];
+//       const result = await Butterfly.find(null, { _id: 0, title: 1, nick: 1 });
+//       res.locals.nav = result;
+//       next();
+
+//     } catch (err) {
+//       throw err;
+//     }
+// };
